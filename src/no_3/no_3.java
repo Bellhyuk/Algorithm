1743ab42c102b1a02e3e18fefa53cfacdf44bd9b
s_kimjh3303 <s_kimjh3303@DESKTOP-5D4KUAE>
2021-04-05 22:43:42 +0900

    20210405 인프론 코테강의 정리

--- src/no_3/no_3.java
+++ src/no_3/no_3.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class no_3{
   //문장 속 단어
-  //한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요
+  //한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력
   public String solution(String str) {
     String answer="";
     int max=Integer.MIN_VALUE, pos;
